fe4a25b478fb286c64705605077ae6b58550cc82
Arnav Mehta <arnavdatt@gmail.com>
2025-02-16 23:49:52 +0000

    Add standard types override

---
+++ Prism/StandardTypes.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Elos/Common/StandardTypes.h>
+
+namespace PX
+{
+	using i8     = Elos::i8;
+	using i16    = Elos::i16;
+	using i32    = Elos::i32;
+	using i64    = Elos::i64;
+	using u8     = Elos::u8;
+	using u16    = Elos::u16;
+	using u32    = Elos::u32;
+	using u64    = Elos::u64;
+	using byte   = Elos::byte;
+	using char16 = Elos::char16;
+	using char32 = Elos::char32;
+	using f32    = Elos::f32;
+	using f64    = Elos::f64;
+}
